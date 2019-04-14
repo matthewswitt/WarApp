a22d6f43b9c3f35a0d56a0db4a8a45aaecba8e41
Matthew Switt <matthewswitt@yahoo.com>
2019-04-14 10:26:00 -0400

    Initial Commit

---
+++ Switt_Matthew_WAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Switt_Matthew_WAR
+//
+//  Created by Matthew Switt on 2019-04-14.
+//  Copyright © 2019 Matthew Switt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
